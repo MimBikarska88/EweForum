b09b6c7bf990ccb32a7d6b676b0f5bd4327034b2
Mimi <mariabikarska2988@gmail.com>
2024-03-02 23:55:33 +0200

    Add context and scaffold identity

---
+++ EweForum/Data/EweDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EweForum.Infrastructure.Data
+{
+    public class EweDbContext :DbContext 
+    {
+        public EweDbContext(DbContextOptions options) :base(options) { }
+       
+    }
+}
